47cca465ffe8cefc2cac1de6ef769732bd0c446a
Mathias Punkenhofer <mpunkenhofer@student.tugraz.at>
2018-01-07 13:13:12 +0100

    added: setup.py

---
+++ __about__.py
@@ -0,0 +1,16 @@
+# Mathias Punkenhofer
+# code.mpunkenhofer@gmail.com
+#
+
+__all__ = ["__title__", "__summary__", "__uri__", "__version__", "__author__", "__email__", "__license__"]
+
+__title__ = "pychess"
+__summary__ = "A simple library for chess based applications"
+__uri__ = "https://github.com/mpunkenhofer/pychess"
+
+__version__ = "0.0.2"
+
+__author__ = "Mathias Punkenhofer"
+__email__ = "code.mpunkenhofer@gmail.com"
+
+__license__ = "MIT"
---
+++ setup.py
@@ -0,0 +1,22 @@
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+from setuptools import setup, find_packages
+
+about = {}
+with open("__about__.py") as fp:
+    exec(fp.read(), about)
+
+setup(
+    name=about['__title__'],
+    version=about['__version__'],
+    description=about['__summary__'],
+    url=about['__uri__'],
+    author=about['__author__'],
+    author_email=about['__email__'],
+    packages=find_packages(exclude=['examples', 'examples.*', 'tests', 'tests.*', 'docs'])
+)
